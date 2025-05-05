aff00eab9a3fa41ae967eaef0a040eaa246e8c8d
Daniel <cranston.daniel@gmail.com>
2025-05-05 11:14:43 +0900

    add 'add_parameter_event_callback' example

--- python_parameter_event_handler/python_parameter_event_handler/parameter_event_handler.py
+++ python_parameter_event_handler/python_parameter_event_handler/parameter_event_handler.py
@@ -4,6 +4,7 @@ from rclpy.node import Node
 import rclpy.parameter
 
 from rclpy.parameter_event_handler import ParameterEventHandler
+from rcl_interfaces.msg import ParameterEvent
 
 
 class SampleNodeWithParameters(Node):
@@ -11,6 +12,7 @@ class SampleNodeWithParameters(Node):
         super().__init__("node_with_parameters")
 
         self.declare_parameter("an_int_param", 0)
+        self.declare_parameter("another_double_param", 0.0)
 
         self.handler = ParameterEventHandler(self)
 
@@ -25,10 +27,25 @@ class SampleNodeWithParameters(Node):
             callback=self.callback,
         )
 
+        self.callback_handle3 = self.handler.add_parameter_event_callback(
+            callback=self.event_callback,
+        )
+
     def callback(self, p: rclpy.parameter.Parameter) -> None:
         self.get_logger().info(
             f"Received an update to parameter: {p.name}: {rclpy.parameter.parameter_value_to_python(p.value)}"
         )
+
+    def event_callback(self, event: ParameterEvent):
+        """
+        ros2 service call /node_with_parameters/set_parameters_atomically rcl_interfaces/srv/SetParametersAtomically "parameters: [{name: an_int_param, value: {type: 2, integer_value: 1}}, {name: an_int_param, value: {type: 2, integer_value: 1}}]"
+        """
+        self.get_logger().info("Received parameter event")
+
+        for p in event.changed_parameters:
+            self.get_logger().info(
+                f"Inside event: {p.name} changed to: {rclpy.parameter.parameter_value_to_python(p.value)}"
+            )
 
 
 def main():
